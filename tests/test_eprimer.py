28579a77925d2539418936c74587fe4f0c51f458
Ru Cindrea <ext-ru.cindrea@vaisala.com>
2022-01-13 18:37:38 +0200

    wip

--- tests/test_eprimer.py
+++ tests/test_eprimer.py
@@ -2,3 +2,9 @@ from playwright.sync_api import Page
 
 def test_word_text(page: Page):
     page.goto('https://www.exploratorytestingacademy.com/app')
+    page.fill("#inputtext", "to be")
+    page.click("#CheckForEPrimeButton")
+    
+    assert page.inner_text("#wordCount") == "1"
+    assert page.inner_text("#discouragedWordCount") == "1"
+    assert page.inner_text("#possibleViolationCount") == "0"
